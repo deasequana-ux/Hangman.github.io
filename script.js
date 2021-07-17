c71c58eadc3bd69403fcef2c4d89de73e1dde8ea
DeaSequana <yucehilal99@gmail.com>
2021-07-17 21:45:39 +0300

    dosyalar yuklendi

---
+++ script.js
@@ -0,0 +1,114 @@
+
+const word_el = document.getElementById("word");
+const popup = document.getElementById('popup-container');
+const message_el = document.getElementById("success-message");
+const wrongLetters_el = document.getElementById("wrong-letters");
+const items = document.querySelectorAll('.item');
+const message = document.getElementById('message');
+const playAgainbBtn = document.getElementById('play-again');
+const score_el = document.getElementById('score');
+ 
+const correctLetters = [];
+const wrongLetters= [];
+let selectedWord = getRandomWord();
+
+function getRandomWord(){
+    const words = ["javascrıpt","java","python","pascal","php","swift","ruby"];
+
+    return words[Math.floor(Math.random() * words.length)]; //kelime uzunluğu kadar random tam sayı üretimi
+}
+
+
+function displayWord(){
+
+    word_el.innerHTML = `
+        ${selectedWord.split('').map(letter => `
+            <div class="letter">
+                ${correctLetters.includes(letter) ? letter: ''}
+            </div>
+        `).join('')}
+
+    `;
+
+   const w = (word_el.innerText.replace(/\n/g,''));
+
+   if(w === selectedWord){
+        popup.style.display = 'flex';
+        message_el.innerText = 'Won!';
+        total_score();
+   }
+}
+
+function updateWrongLetters(){
+    wrongLetters_el.innerHTML = `
+        ${wrongLetters.length>0?'<h3>Wrong Letters</h3>':''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+    items.forEach((item,index) => {
+        const errorCount= wrongLetters.length;
+        if(index < errorCount){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none';
+        }
+        
+    });
+
+    if(wrongLetters.length === items.length){
+        popup.style.display = 'flex';
+        message_el.innerText = 'Lose!';
+        total_score();
+    }
+}
+
+function total_score(){
+    let start_score = 30;
+    for(i = 0; i < wrongLetters.length; i++){
+        if(wrongLetters+1){
+            start_score = start_score - 5;
+        }
+    }
+    score_el.innerHTML = `Total Score:${start_score}`;
+}
+
+function displayMessage(){
+    message.classList.add('show');
+
+    setTimeout(function(){
+        message.classList.remove('show');
+    },2000);
+}
+
+playAgainbBtn.addEventListener('click',function(){
+    correctLetters.splice(0); //dizi sıfırlanır.
+    wrongLetters.splice(0);
+
+    selectedWord = getRandomWord();
+    displayWord();
+    updateWrongLetters();
+
+    popup.style.display='none';
+});
+
+window.addEventListener('keydown', function(e){
+    if(e.keyCode >= 65 && e.keyCode <= 90){
+        const letter = e.key;
+        if(selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter);
+                displayWord();
+            }else{
+                displayMessage();
+            }
+        }else{
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter);
+                updateWrongLetters();
+            }else{
+                displayMessage();
+            }
+        }
+    }
+});
+
+displayWord()
